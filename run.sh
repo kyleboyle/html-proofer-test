1ffed3134eef57782b9792adf70a2afc1a9f3de7
Kyle Boyle <kyle@kyleboyle.com>
2015-02-20 15:34:33 -0400

    gem install

--- run.sh
+++ run.sh
@@ -6,12 +6,17 @@ else
     export WERCKER_HTML_PROOFER_TEST_VERSION="-v 2.1.0"
 fi
 
+export OLD_GEM_HOME=$GEM_HOME
+
+# install ruby gem
+mkdir $WERCKER_STEP_ROOT/gems
+export GEM_HOME=$WERCKER_STEP_ROOT/gems
+gem install html-proofer $WERCKER_HTML_PROOFER_TEST_VERSION
+
 
 cd $WERCKER_SOURCE_DIR/$WERCKER_HTML_PROOFER_TEST_BASEDIR
 
-gem install html-proofer $WERCKER_HTML_PROOFER_TEST_VERSION
-
-htmlproof --verbose
+$WERCKER_STEP_ROOT/gems/bin/htmlproof --verbose
 
 #export ARGS='{:href_swap => {/http:\/\/baseurl\.com/ => ""} }'
 #ruby -e "require 'html/proofer'" -e "HTML::Proofer.new('.', $ARGS).run"
